6c987869ec8fad1a9878255cb0298df552313368
Keiko Iwata <oumelab@pt.rdy.jp>
2025-04-18 20:33:05 +0900

    サイトレイアウトの実装

---
+++ src/components/card.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+type CardProps = {
+  children: React.ReactNode;
+  hoverShadow?: boolean;
+};
+
+export default function Card(props: CardProps) {
+  const hoverStyle = props.hoverShadow
+    ? "hover:shadow-lg transition-shadow duration-300"
+    : "";
+
+  return (
+    <div
+      className={`rounded-xl border w-full p-6 space-y-8 bg-gradient-to-br from-slate-50 to-sky-100 border-sky-500 ${hoverStyle}`}
+    >
+      {props.children}
+    </div>
+  );
+}
