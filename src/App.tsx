b6567e6cc91d82064bf3fe269e2b29b66e2046e0
Keiko Iwata <oumelab@pt.rdy.jp>
2025-04-18 20:49:08 +0900

    イベント詳細ページの作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./components/layout";
 import EventList from "./pages/event-list";
+import EventDetail from "./pages/event-detail";
 
 export default function App() {
   return (
@@ -11,7 +12,7 @@ export default function App() {
         <Route path="events" element={<Layout />}>
           <Route index element={<EventList />} />
           {/* 動的な値に応じたURL */}
-          <Route path=":id" element={<p>イベント詳細を表示</p>} />
+          <Route path=":id" element={<EventDetail />} />
           <Route path=":id/apply" element={<p>イベントに申し込む</p>} />
           <Route path=":id/confirm" element={<p>申し込み完了です</p>} />
         </Route>
---
+++ src/pages/event-detail.tsx
@@ -0,0 +1,66 @@
+import { useParams, Link } from "react-router-dom";
+import { EVENTS as events } from "../constants";
+import REACT_EVENT from "../assets/react-event.jpg";
+import { CalendarDays, MapPin, Users } from "lucide-react";
+import Card from "../components/card";
+export default function EventDetail() {
+  const { id } = useParams();
+  const event = events.find((e) => e.id === id);
+
+  if (!event) {
+    return (
+      <>
+        <p>URLのイベントが見つかりません</p>
+        <Link to="/" className="underline">イベント一覧に戻る</Link>
+      </>
+    );
+  }
+
+  return (
+    <div className="flex gap-8 max-w-5xl mx-auto">
+    <Card>
+      <h2 className="text-4xl font-bold mb-4 text-gray-800">{event.title}</h2>
+      <p className="whitespace-pre-wrap">{event.description}</p>
+      <img
+        src={REACT_EVENT}
+        alt={event.title}
+        className="rounded-t-lg w-[660px] h-[440px]"
+      />
+    </Card>
+    <div className="">
+      <Card>
+        <h2 className="text-2xl font-bold mb-4 text-gray-800">
+          イベントの詳細
+        </h2>
+        <div className="flex flex-wrap gap-4 text-base">
+          <div className="flex items-center">
+            <CalendarDays className="w-4 h-4 mr-2 text-blue-500" />
+            <span>{event.date}〜</span>
+          </div>
+          <div className="flex items-center">
+            <MapPin className="w-4 h-4 mr-2 text-green-500" />
+            <span>{event.location}</span>
+          </div>
+          <div className="flex items-center">
+            <Users className="w-4 h-4 mr-2 text-purple-500" />
+            <span>
+              {event.attendees}
+              {event.capacity && `/${event.capacity}`}
+              人参加予定
+            </span>
+          </div>
+        </div>
+
+        <Link to={`/events/${event.id}/apply`} className="block">
+          <button
+            // onClick={() => navigate(`/events/${event.id}/apply`)}
+            className="py-4 w-full bg-sky-600 hover:opacity-80 text-white rounded-xl"
+          >
+            申し込む
+          </button>
+        </Link>
+      </Card>
+    </div>
+  </div>
+  );
+}
