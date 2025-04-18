e9da26a86ad87bf3a56c49e91621e7b228c60fb5
Keiko Iwata <oumelab@pt.rdy.jp>
2025-04-18 21:18:51 +0900

    イベント申し込みフローの実装 - イベント申し込みページの作成 - 申し込み完了ページの作成

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./components/layout";
 import EventList from "./pages/event-list";
 import EventDetail from "./pages/event-detail";
+import EventApply from "./pages/event-apply";
+import EventConfirm from "./pages/event-confirm";
 
 export default function App() {
   return (
@@ -13,8 +15,8 @@ export default function App() {
           <Route index element={<EventList />} />
           {/* 動的な値に応じたURL */}
           <Route path=":id" element={<EventDetail />} />
-          <Route path=":id/apply" element={<p>イベントに申し込む</p>} />
-          <Route path=":id/confirm" element={<p>申し込み完了です</p>} />
+          <Route path=":id/apply" element={<EventApply />} />
+          <Route path=":id/confirm" element={<EventConfirm />} />
         </Route>
         {/* 存在しないURLの処理 */}
         <Route path="*" element={<p>404 not found</p>} />
